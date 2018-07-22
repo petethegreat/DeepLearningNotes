d43ad53f2c47c5fe76728e01df13cd2b648ff501
Peter Thompson <pthompson@loyalty.com>
2018-07-22 14:11:22 -0400

    initial pdf

--- activations.py
+++ activations.py
@@ -50,7 +50,7 @@ def act_leaky_ReLu(xmin,xmax):
     x = np.linspace(xmin,xmax,100)
     y = np.where(x >= 0,x,0.01*x)
     derv = np.where(x>=0,1,0.01)
-    names = (r'y = 0.01x (x< 0) , x (x >= 0)',r'0.01 (x <0) , 1 (x >=0)','leaky ReLu')
+    names = (r'y = 0.01x (x< 0) , x (x >= 0)',r'0.01 (x <0) , 1 (x >=0)','leaky_ReLu')
     return x,y,derv,names
 
 
